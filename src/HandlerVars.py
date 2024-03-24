7657f2421e75e2e6c5bd6bb8f538a28f40f6a425
Kabir Akzaman <62380128+Kabir404@users.noreply.github.com>
2024-03-24 03:55:54 +0000

    Fix Unexpected brackets(most likely from IDE)

--- src/HandlerVars.py
+++ src/HandlerVars.py
@@ -15,9 +15,7 @@ class ErrorInfo:
 
 # Here are the crash handler configuration options.
 ProductName = "Program"  # Here you specify the formalized name of the program.
-DeveloperName = (
-    "Developer"  # Here you specify the name of the developer of the program.
-)
+DeveloperName = "Developer"  # Here you specify the name of the developer of the program.
 BinaryPath = "/bin/echo 0"  # Here you specify the startup path for the executable.
 LogsPath = "./logs/"  # Here you can specify where to keep logs for it.
 Version = "0.0.0.0"  # Here you specify the Version of the program.
